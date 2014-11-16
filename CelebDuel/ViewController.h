ec982c9357a8e3a44d8bc7ac39c46f5a1232eea4
Kervins Valcourt <kervins.valcourt@gmail.com>
2014-11-15 20:37:42 -0500

    Initial Commit

---
+++ CelebDuel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CelebDuel
+//
+//  Created by Kervins Valcourt on 11/15/14.
+//  Copyright (c) 2014 EastoftheWestEnd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

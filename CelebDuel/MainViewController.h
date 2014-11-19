5beb6f2f3c46d6822e40ce9180752d087239c427
Kervins Valcourt <kervins.valcourt@gmail.com>
2014-11-19 13:36:39 -0500

    first commit

--- CelebDuel/MainViewController.h
+++ CelebDuel/MainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  CelebDuel
 //
 //  Created by Kervins Valcourt on 11/15/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainViewController : UIViewController
 
 @end
-

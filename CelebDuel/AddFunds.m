d51403d0d06ae26a1a05fae60ab2fe0344918a17
Jesse Schneider <jmschneider430@gmail.com>
2014-11-30 23:34:19 -0500

    Simply added a comment to the cancel button on the add funds view

--- CelebDuel/AddFunds.m
+++ CelebDuel/AddFunds.m
@@ -29,6 +29,8 @@
     self.statesTextField.inputView = picker;
     self.theStates = @[@"Alabama", @"Alaska", @"American Samoa", @"Arizona", @"Arkansas", @"California", @"Colorado", @"Connecticut", @"Delaware", @"District of Columbia", @"Florida", @"Georgia", @"Hawaii", @"Idaho", @"Illinois", @"Indiana", @"Iowa", @"Kansas", @"Kentucky", @"Louisiana", @"Maine", @"Maryland", @"Massachusetts", @"Michigan", @"Minnesota", @"Mississippi", @"Missouri", @"Montana", @"Nebraska", @"Nevada", @"New Hampshire", @"New Jersey", @"New Mexico", @"New York", @"North Carolina", @"North Dakota", @"Ohio", @"Oklahoma", @"Oregon", @"Pennsylvania", @"Puerto Rico", @"Rhode Island", @"South Carolina", @"South Dakota", @"Tennessee", @"Texas", @"Utah", @"Vermont", @"Virgin Islands", @"Virgina", @"Washington", @"West Virgina", @"Wisconsin",@"Wyoming",];
 }
+
+//Cancel Button
 - (IBAction)cancelDidFired:(id)sender {
     
     [self dismissViewControllerAnimated:YES completion:nil];

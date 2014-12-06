646fc2cba7cbeebe091ff753a2133bd2fc86ec4e
Jesse Schneider <jmschneider430@gmail.com>
2014-12-06 15:28:47 -0500

    credit card view

--- CelebDuel/CreditCardInfoView.m
+++ CelebDuel/CreditCardInfoView.m
@@ -19,6 +19,13 @@
     // Do any additional setup after loading the view from its nib.
 }
 
+//Cancel Button
+- (IBAction)cancelButton:(id)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
